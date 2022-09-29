469cc0130bd9dfc8482507bb8c67840e27581974
anwargates <102929799+anwargates@users.noreply.github.com>
2022-09-29 17:43:12 +0700

    sesi 11 done

---
+++ sesi-11/materi/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import AuthLoginForm from "./features/AuthLoginForm";
+// import UsersList from "./features/UsersList"
+
+function App() {
+  return (
+    <main className="App">
+      {/* <UsersList /> */}
+      <AuthLoginForm />
+    </main >
+  )
+}
+
+
+export default App;
